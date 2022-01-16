31b48ceb7783d00f106c7c041bbeb437999b8667
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2022-01-16 15:58:53 -0300

    feat: create contact controller

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,23 @@
+class ContactController {
+  index(request, response) {
+    response.send('Send from contact controller');
+  }
+
+  show() {
+
+  }
+
+  store() {
+
+  }
+
+  update() {
+
+  }
+
+  delete() {
+
+  }
+}
+
+module.exports = new ContactController();
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
+const routes = require('./routes');
+
 const app = express();
 
-app.get('/', (request, response) => {
-  response.send('Hello world');
-});
+app.use(routes);
 
 app.listen(3333, () => console.log('🔥 Server started at http://localhost:3333'));
