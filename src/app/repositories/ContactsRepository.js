a77c0f9318981eb0484a22c9f193eea5a15213f2
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2022-01-16 22:32:56 -0300

    refact: refact create method in Contacts Repository

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,5 +1,7 @@
 const { v4: uuid } = require('uuid');
 
+const db = require('../../database');
+
 let contacts = [
   {
     id: uuid(),
@@ -27,21 +29,16 @@ class ContactsRepository {
     ));
   }
 
-  create({
+  async create({
     name, email, phone, category_id,
   }) {
-    return new Promise((resolve) => {
-      const newContact = {
-        id: uuid(),
-        name,
-        email,
-        phone,
-        category_id,
-      };
+    const [row] = await db.query(`
+      INSERT INTO contacts (name, email, phone, category_id )
+      VALUES ($1, $2, $3, $4)
+      RETURNING *;
+    `, [name, email, phone, category_id]);
 
-      contacts.push(newContact);
-      resolve(newContact);
-    });
+    return row;
   }
 
   update(id, {
