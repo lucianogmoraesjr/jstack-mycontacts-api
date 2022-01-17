58d32fb602da3b54cf3aa8bac737c6bafb34db8b
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2022-01-17 17:09:11 -0300

    feat: create error handler

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 
 const routes = require('./routes');
+require('express-async-errors');
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+// eslint-disable-next-line no-console
+  console.log(error);
+  response.sendStatus(500);
+});
 
 // eslint-disable-next-line no-console
 app.listen(3333, () => console.log('🔥 Server started at http://localhost:3333'));
