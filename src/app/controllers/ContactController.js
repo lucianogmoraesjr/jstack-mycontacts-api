e0b8fc9f2670181c19e6cf4bf7ff8e020975363c
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2022-02-15 07:53:29 -0300

    refact: status code in category controller

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -15,7 +15,7 @@ class ContactController {
     const contact = await ContactsRepository.findById(id);
 
     if (!contact) {
-      return response.status(404).json({ error: 'User not found' });
+      return response.status(404).json({ error: 'Contact not found' });
     }
 
     response.json(contact);
@@ -43,7 +43,7 @@ class ContactController {
       category_id,
     });
 
-    response.json(contact);
+    response.status(201).json(contact);
   }
 
   async update(request, response) {
@@ -55,7 +55,7 @@ class ContactController {
     const contactExists = await ContactsRepository.findById(id);
 
     if (!contactExists) {
-      return response.status(404).json({ error: 'User not found' });
+      return response.status(404).json({ error: 'Contact not found' });
     }
 
     const contactEmailInUse = await ContactsRepository.findByEmail(email);
